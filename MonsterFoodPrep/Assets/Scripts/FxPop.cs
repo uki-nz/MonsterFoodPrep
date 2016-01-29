61d77687c20f744d66b3658bed3d980ca53f4eaa
Kit <kits@email.lol>
2016-01-29 22:35:05 +1300

    Adding a tweening library and special effects for monster death

---
+++ MonsterFoodPrep/Assets/Scripts/FxPop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FxPop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        iTween.ScaleTo(gameObject, Vector3.one, 0.5f);
+        StartCoroutine(Kill());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Kill()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+}
