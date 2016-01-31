d2d7345e122b42b86e6c26c5104d17056f7abebe
uki <contact@uki.nz>
2016-01-31 16:04:31 +1300

    tutorial potato

---
+++ MonsterFoodPrep/Assets/Scripts/TutorialLine.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialLine : MonoBehaviour
+{
+    Transform parent;
+    Vector3 position;
+    Quaternion rotation;
+
+    void Awake()
+    {
+        parent = transform.parent;
+        position = transform.localPosition;
+        rotation = transform.localRotation;
+        transform.parent = null;
+    }
+
+    void Update()
+    {
+        transform.position = parent.position + position;
+        transform.rotation = rotation;
+    }
+}
